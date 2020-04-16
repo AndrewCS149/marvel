497da83a426118db71511b86c2134f75f6b27402
AndrewCS149 <Andrewcs149@gmail.com>
2020-04-16 14:12:14 -0700

    created function to swap header image with prompt

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
+var header = document.getElementById("mainHeader");
+
 function avengerZone() {
   confirm("You are about to enter the avenger's zone");
 }
 
 function strongestAvenger() {
   var avenger = prompt("Who is the strongest avenger?").toLowerCase();
+
   if (avenger == "iron man" || avenger == "ironman") {
     alert("You are correct.");
   } else if (avenger == "hawkeye") {
     alert("He is the weakest...");
+  } else if (avenger == "thor") {
+    header.style.backgroundImage =
+      "url(https://pbs.twimg.com/media/D1qIO0KWkAE4NVY.jpg)";
+  } else if (avenger == "loki") {
+    header.style.backgroundImage =
+      "url(https://coverfiles.alphacoders.com/465/46591.jpg)";
+  } else if (
+    avenger == "spiderman" ||
+    avenger == "spider man" ||
+    avenger == "spider-man"
+  ) {
+    header.style.backgroundImage =
+      "url(https://data.whicdn.com/images/333714008/original.jpg)";
   } else {
     alert("WRONG");
   }
