0d71ca912460f382f3fe3a7bceb599aa735a5a6d
AndrewCS149 <Andrewcs149@gmail.com>
2020-04-16 12:26:07 -0700

    wrapped code in functions

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
-confirm("You are about to enter the avenger's zone")
-
-var avenger = prompt("Who is the strongest avenger?");
-
-avenger = avenger.toLowerCase();
-
-if (avenger == "iron man" || avenger == "ironman") {
-    alert("You are correct.");
-} else {
-    alert("WRONG");
+function avengerZone() {
+  confirm("You are about to enter the avenger's zone");
 }
+
+function strongestAvenger() {
+  var avenger = prompt("Who is the strongest avenger?").toLowerCase();
+  if (avenger == "iron man" || avenger == "ironman") {
+    alert("You are correct.");
+  } else if (avenger == "hawkeye") {
+    alert("He is the weakest...");
+  } else {
+    alert("WRONG");
+  }
+}
+
+avengerZone();
+strongestAvenger();
